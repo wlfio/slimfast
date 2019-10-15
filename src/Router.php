459fae96da3ac7bf3f9c0d72ec88d7a3ef3b53e0
WolfGang <wolfgang@voncaron.co.uk>
2019-10-15 17:08:49 +0100

    stoof

---
+++ src/Router.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace WlfIO\SlimFast;
+
+use Slim\App as SlimApp;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class Router
+{
+    private static $instances = [];
+
+    private $routes = [];
+
+    public static function Instance(string $path): Router
+    {
+        $path = realpath($path);
+        if (empty(self::$instances)) {
+            $class = get_called_class();
+            self::$instances[$path] = new $class($path);
+        }
+        return self::$instances[$path];
+    }
+
+    public function __construct(string $path)
+    {
+        $this->gatherRoutes($path);
+    }
+
+    private function gatherRoutes(string $path)
+    {
+        $path = realpath($path);
+        if (file_exists($path)) {
+            foreach (new \DirectoryIterator($path) as $file) {
+                if($file->isDot())continue;
+                if($file->isFile() && $file->getExtension() == "php"){
+                    $this->loadRouteFile($file->getRealPath());
+                } else if($file->isDir()){
+                    $this->gatherRoutes($file->getRealPath());
+                }
+            }
+        }
+    }
+
+    private function loadRouteFile(string $filePath){
+        require $filePath . "";
+    }
+
+    public function addRoute($method, string $pattern, array $callback, $data)
+    {
+        $this->routes[] = [
+            "slim" => [
+                $method,
+                $pattern,
+                $callback
+            ],
+            "data" => $data
+        ];
+        return $this;
+    }
+
+    public function populateRoutes(SlimApp $app)
+    {
+        $app->map(
+            ["GET"],
+            "/ping",
+            function(Request $request, Response $response, $args){
+                $response->getBody()->write("pong");
+                return $response;
+            }
+        );
+
+        foreach ($this->routes as $route) {
+            $app->map(...$route["slim"]);
+        }
+    }
+}
---
+++ src/SlimFast.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace WlfIO\SlimFast;
+
+use DI\Container;
+use DI\ContainerBuilder;
+use Slim\App;
+use Slim\Factory\AppFactory;
+
+class SlimFast
+{
+    const APP_START = "APP_START";
+    const APP_ROOT = "APP_ROOT";
+    const APP_NAME = "APP_NAME";
+    const SLIMFAST_ROOT = "SLIMFAST_ROOT";
+    const DEBUG_ENABLED = "DEBUG_ENABLED";
+
+    private static $instance;
+
+    /** @var App */
+    private $app;
+    private $container = null;
+
+    public static function Instance() : SlimFast
+    {
+        if (empty(self::$instance)) {
+            $class = get_called_class();
+            self::$instance = new $class();
+        }
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        $this->setup();
+    }
+
+    protected function setup()
+    {
+        self::Defines();
+
+        $this->setupContainer();
+
+        $this->createSlim();
+
+        $this->setupMiddleware();
+
+        $this->populateRoutes();
+    }
+
+    protected function setupContainer(){
+        $builder = new ContainerBuilder();
+
+        $this->container["settings"]["debug"] = DEBUG_ENABLED;
+
+        $this->populateContainerAliases($builder);
+
+        $this->container = $builder->build();
+    }
+
+    protected function populateRoutes($path = null){
+        $path = $path ?? APP_ROOT . "src/routes";
+        Router::Instance($path)->populateRoutes($this->app);
+    }
+
+    protected function getContainerAliases(){
+        return [];
+    }
+
+    private function populateContainerAliases(ContainerBuilder $builder){
+        foreach($this->getContainerAliases() as $alias => $class){
+            if($alias !== $class){
+                $builder->addDefinitions()[$alias] = function (Container $c) use ($class) {
+                    return $c->get($class);
+                };
+            }
+        }
+    }
+
+    protected function setupMiddleware(){
+
+    }
+
+    private function createSlim()
+    {
+        AppFactory::setContainer($this->container);
+        $this->app = AppFactory::create();
+    }
+
+    final public function getContainer() {
+        return $this->container;
+    }
+    
+    public static function Defines(array $params = [])
+    {
+        defined(self::APP_START) or define(self::APP_START, $params[self::APP_START] ?? microtime(true));
+        defined(self::SLIMFAST_ROOT) or define(self::SLIMFAST_ROOT, realpath($params[self::SLIMFAST_ROOT] ?? __DIR__ . "/../"));
+        defined(self::APP_ROOT) or define(self::APP_ROOT, realpath($params[self::APP_ROOT] ?? __DIR__ . "/../../../../"));
+        defined(self::APP_NAME) or define(self::APP_NAME, $params[self::APP_NAME] ?? "SlimFastApp");
+        defined(self::DEBUG_ENABLED) or define(self::DEBUG_ENABLED, ($params[self::DEBUG_ENABLED] ?? false) === true);
+    }
+}
